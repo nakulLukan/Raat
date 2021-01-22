b8874af619d4afeae74ac0151ab505cc09909582
nakul <nakul.a@experionglobal.com>
2021-01-22 21:15:04 +0530

    Initial project setup

---
+++ Raat.Web/Pages/Index.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Raat.Web.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        public string WelcomeText = "Welcome to RaaT";
+    }
+}
