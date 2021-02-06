c15be57685997f16a8bc2b8ac0a20f5a7383d83b
nakul <nakul.a@experionglobal.com>
2021-02-06 19:56:26 +0530

    Balzor wasm debug issues fixed

--- Raat.Web/Pages/Index.razor.cs
+++ Raat.Web/Pages/Index.razor.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Raat.Web.Pages
@@ -9,5 +10,15 @@ namespace Raat.Web.Pages
     public class IndexBase : ComponentBase
     {
         public string WelcomeText = "Welcome to RaaT";
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        protected void Click()
+        {
+            Console.WriteLine("Clicked me");
+        }
     }
 }
--- Raat.Web/Program.cs
+++ Raat.Web/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Raat.Web
@@ -15,7 +12,7 @@ namespace Raat.Web
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
